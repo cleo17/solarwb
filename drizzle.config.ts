b629b61b70cfccefb2acbd7aabe6b72ace732943
cleo17 <cleophaschebii@gmail.com>
2025-05-29 14:54:08 +0300

    Update: fixed DB config and other improvements

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default {
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/solartech",
+    url: process.env.DATABASE_URL || "postgresql://solarwb_db_user:postgres@dpg-d0s47cm3jp1c73ealjtg-a:5432/solarwb_db",
   },
 } satisfies Config;
